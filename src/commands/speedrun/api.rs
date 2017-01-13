9650a01aa910003888eacf01b021d6fda39d822f
cronokirby <cronokirby@gmail.com>
2017-01-13 23:10:20 +0100

    Filled out CategorieFilled out Categoriess

--- src/commands/speedrun/api.rs
+++ src/commands/speedrun/api.rs
@@ -1,4 +1,5 @@
-
+extern crate hyper;
+use self::hyper::error::Result;
 
 extern crate rustc_serialize;
 use self::rustc_serialize::json::{Json};
@@ -9,22 +10,29 @@ use http;
 type Url = String;
 
 #[derive (Debug)]
-struct Category {
-    name: String
+pub struct Category {
+    name: String,
+    url: Url
 }
+
 
 // This acts as a helper function to `parse_categories`, getting mapped over an array
 fn parse_category(json: &Json) -> Category {
-    let name = json.as_object().unwrap()
-                   .get("name").unwrap()
+    let data = json.as_object().unwrap();
+    let name = data.get("name").unwrap()
                    .as_string().unwrap();
-    Category{ name: name.to_string() }
+    let url = data.get("links").unwrap()
+                  .as_array().unwrap()[3]
+                  .as_object().unwrap()
+                  .get("uri").unwrap()
+                  .as_string().unwrap();
+    Category{ name: name.to_string()
+            , url: url.to_string() }
 }
 
 
 // This function isn't really safe, but an error in json parsing is relatively rare
-// Call this with `catch_unwind`, because of that.
-fn parse_categories(json_string: &str) -> Option<Vec<Category>> {
+fn parse_categories(json_string: &str) -> Vec<Category> {
     // The use of 2 variables here is to extend the lifetime of `data`
     let data = Json::from_str(json_string).unwrap();
     let category_data = data.as_object().unwrap()
@@ -33,7 +41,13 @@ fn parse_categories(json_string: &str) -> Option<Vec<Category>> {
     let categories = category_data.iter()
                             .map(|json| parse_category(&json))
                             .collect();
-    Some(categories)
+    categories
+}
+
+
+pub fn fetch_categories(url: &str) -> Result<Vec<Category>> {
+    let json = http::get(url);
+    json.map(|str| parse_categories(&*str))
 }
 
 
---
+++ src/commands/speedrun/commands.rs
@@ -0,0 +1,6 @@
+extern crate serenity;
+use self::serenity::client::Context;
+use self::serenity::model::{ChannelId, User};
+
+use commands::messages::send_min_embed;
+use commands::speedrun::api;
