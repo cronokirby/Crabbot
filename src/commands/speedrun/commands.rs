41208029705d963b73798b1d1d85a7d5a2a38c30
cronokirby <cronokirby@gmail.com>
2017-01-14 00:16:03 +0100

    Got a basic implementation of the categories command going

--- src/commands/speedrun/commands.rs
+++ src/commands/speedrun/commands.rs
@@ -1,6 +1,18 @@
 extern crate serenity;
 use self::serenity::client::Context;
-use self::serenity::model::{ChannelId, User};
+use self::serenity::model::ChannelId;
 
 use commands::messages::send_min_embed;
 use commands::speedrun::api;
+
+
+pub fn categories<C>(context: Context, channel_id: C, words: &Vec<&str>)
+    where C: Into<ChannelId>{
+    let abbreviation = words[1];
+    let url = format!("http://www.speedrun.com/api/v1/games/{}/categories",
+                      abbreviation);
+    let categories = api::fetch_categories(&*url);
+    let response = format!("Here's a list of categories for {}:\n{:?}",
+                           abbreviation, categories);
+    send_min_embed(context, channel_id, &*response);
+}
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use serenity::Client;
 
 extern crate crabbot;
 use crabbot::commands;
-use crabbot::commands::speedrun::api as speedrun;
+use crabbot::commands::speedrun::commands as speedrun;
+
 
 fn main() {
-    speedrun::testo();
     // Configure the client to use a preset token
     let token = env::var("DISCORD_TOKEN1")
         .expect("Expected a token in the environment");
@@ -20,11 +20,13 @@ fn main() {
     client.on_message(|context, message| {
         let author = message.author;
         let channel = message.channel_id;
+        let words: Vec<&str> = message.content.split_whitespace().collect();
         // Match against the first word of a message
-        let prefix = message.content.split_whitespace().nth(0);
+        let prefix = words.get(0);
         match prefix {
-            Some("<test") => commands::test(context, channel, author),
-            Some("<number") => commands::number(),
+            Some(&"<test") => commands::test(context, channel, author),
+            Some(&"<number") => commands::number(),
+            Some(&"<categories") => speedrun::categories(context, channel, &words),
             _ => {}
         }
     });
@@ -33,7 +35,6 @@ fn main() {
     client.on_ready(|_context, ready| {
         println!("Connected as: {}", ready.user.name);
     });
-
 
     // Starting the bot
     if let Err(why) = client.start() {
